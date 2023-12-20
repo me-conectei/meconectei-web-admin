aa1410fb2faeecb1d3266d4b4f31b8126847f4f3
Jeanluca <jeanlucafp@gmail.com>
2023-12-20 18:54:17 -0300

    Update Modal.js
    corrigido envio de email

--- src/pages/Companies/components/Modal.js
+++ src/pages/Companies/components/Modal.js
@@ -16,6 +16,7 @@ import {
   CardContent,
   TextField,
 } from "@material-ui/core";
+import { baseURL } from "api";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -49,13 +50,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ModalDialog({idCompany}) {
- // const { isLoading, startLoading, finishLoading } = useSessionContext();
+export default function ModalDialog({ idCompany }) {
+  // const { isLoading, startLoading, finishLoading } = useSessionContext();
   const [open, setOpen] = React.useState(false);
   const [name, setName] = useState("");
   const [surname, setSurname] = useState('')
   const [email, setEmail] = useState("");
-  const [password]= useState('adminpassword')
+  const [password] = useState('adminpassword')
 
   const styles = useStyles();
 
@@ -68,67 +69,71 @@ export default function ModalDialog({idCompany}) {
   };
 
   const addUser = () => {
-    if(!name) {
+    if (!name) {
       toast.error("Por favor preencha o nome");
       return;
     }
-    if(!surname) {
+    if (!surname) {
       toast.error("Por favor preencha o sobrenome");
       return;
     }
-    if(!email) {
+    if (!email) {
       toast.error("Por favor preencha o e-mail");
       return;
     }
+
+    toast.info("Processando informações, aguarde um pouquinho.")
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then((users) => {
-        firebase.auth().sendPasswordResetEmail(email).then(()=>{
+        firebase.auth().sendPasswordResetEmail(email).then(() => {
           console.log('email enviado')
           const body = {
             uidUser: users.user.uid,
             name: name,
             surname: surname,
             email: email,
-          };  
+          };
           axios.post(
-            `https://api-ieaqui.avamobile.com.br/admin/companiesAdmin/${idCompany}`,
-            body,{
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem("sessionToken")}`
-              }
-            }).then(()=>{
-              toast.success(`Convite enviado para ${email}`);
-              handleClose()
-            }).catch((err)=>{
-              console.log(err)
-            })  
-        }).catch((err)=>{
+            `${baseURL}/companiesAdmin/${idCompany}`,
+            body, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("sessionToken")}`
+            }
+          }).then(() => {
+            toast.success(`Convite enviado para ${email}`);
+            handleClose()
+          }).catch((err) => {
+            console.log(err)
+            toast.error('Ocorreu um problema inesperado, cheque o console')
+          })
+        }).catch((err) => {
           console.log(err)
-        })      
+          toast.error('Ocorreu um problema inesperado, cheque o console')
+        })
       })
       .catch((err) => {
-        if(err.code === "auth/email-already-in-use")
+        if (err.code === "auth/email-already-in-use")
           toast.error("Este e-mail já foi utilizado na plataforma");
-        else if(err.code === "auth/invalid-email")
+        else if (err.code === "auth/invalid-email")
           toast.error("E-mail inválido");
         else
           toast.error("E-mail inválido para registrar");
         console.log(err);
       });
-   
+
   };
-  
+
   return (
     <React.Fragment>
-        <Button
-            variant="contained"
-            onClick={handleClickOpen}
-            className={styles.buttonAccess}
-          >
-            Adicionar Administrador
-          </Button>
+      <Button
+        variant="contained"
+        onClick={handleClickOpen}
+        className={styles.buttonAccess}
+      >
+        Adicionar Administrador
+      </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Adicionar administrador para acesso a plataforma</DialogTitle>
         <DialogContent>
